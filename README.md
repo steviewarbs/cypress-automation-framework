e428000bc35741bbd6d707dc5997d104602d2bd1
steviewarbs <steviewarbs1@hotmail.co.uk>
2020-04-05 23:09:12 +0100

    initial cypress code

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project Title
+
+cypress-automation-framework
+
+## Getting Started
+
+This Cypress repository has been created to allow users to fork a UI Automation project using the Javascript/Cypress combination.
+
+
+### Prerequisites
+
+* Intermediate knowledge of Javascript
+* JS compatible IDE, such as Microsoft Visual Code 
+* Working internet
+* Access to https://www.cypress.io/
+* Create local file directory 
+* Make sure that you run npm init or have a node_modules folder or package.json
+* run npm install cypress --save-dev
+
+## Running the tests
+
+To execute tests the following commands need to be executed:
+
+* To Open the Cypress UI runner execute - ./node_modules/.bin/cypress open
+* To Run the Cypress tests in CI execute - ./node_modules/.bin/cypress run
+* To Run the Cypress tests in CI and record for the Dashboard execute - ./node_modules/.bin/cypress run --record
+
+
+## Built With
+
+* Language:Javascript
+
+## Authors
+
+* **Steve Warburton** 
---
+++ cypress/integration/facebook/facebook_sign_up.js
@@ -0,0 +1,24 @@
+describe('Land and assert Tithebarn URL', () => {
+    it('Launches website homepage', () => {
+
+    //Fill in core information, note this is does not follow DRY and can be wrapped into a function in commands.js
+    //For reference I have added a new cy.{method} in the commands file. The example allows the user to see if elements are visible.
+    //Please see cypress/support/commands.js
+
+      cy.visit("https://www.facebook.com/")
+      cy.get("#u_0_o").type("cypress_test_first_name").should('be.visible') 
+      cy.get("#u_0_q").type("cypress_test_surname").should('be.visible') 
+      cy.get("#u_0_t").type("07*********").should('be.visible') 
+      cy.get("#u_0_y").type("cypress_test_password").should('be.visible') 
+
+      //Sets D.O.B from dropdown menu
+
+      cy.get('#day').select('10')
+      cy.get('#month').select('Aug')
+      cy.get('#year').select('1980')
+
+      //Select Male/Femaale
+
+      cy.get('#u_0_7').click()
+    });
+  });
